8fd81157122bdb108d4d862fcbdbd97c5e49bb68
VinylTheDuster <william.salembien@outlook.com>
2025-12-18 16:43:43 +0100

    Added additional students, CSS continuations, improved certain JS scripts.

--- assets/scripts/tableInit.js
+++ assets/scripts/tableInit.js
@@ -1,28 +1,35 @@
 export default function InitTable(json) {
-
     const tabBody = document.getElementById("table-body");
     tabBody.innerHTML = "";
     
+    // Set rows in tab
     for (const student of json.apprenants) {
-
-        // Create row with id
-        const tr = document.createElement("tr");
-        tr.setAttribute("id", student.id);
-
-        // Create cells
-        const tdLName = document.createElement("td");
-        const tdFName = document.createElement("td");
-        const tdTown = document.createElement("td");
-
-        // Set content in cells
-        tdLName.textContent = student.nom.toUpperCase();
-        tdFName.textContent = student.prenom;
-        tdTown.textContent = student.ville;
-
-        // Add cells in row
-        tr.append(tdLName, tdFName, tdTown);
-
-        // Set rows in tab
-        tabBody.appendChild(tr);
+        tabBody.appendChild(createRow(student));
     }
+}
+
+function createRow(student) {
+    // Create row with id
+    const tr = document.createElement("tr");
+    tr.id = student.id;
+
+    // Create cells
+    const tdNom = document.createElement("td");
+    const tdPrenom = document.createElement("td");
+    const tdVille = document.createElement("td");
+    const tdDetail = document.createElement("td");
+
+    // Set content in cells
+    tdNom.textContent = student.nom.toUpperCase();
+    tdPrenom.textContent = student.prenom;
+    tdVille.textContent = student.ville;
+
+    const tdDetailDiv = document.createElement("div");
+    tdDetailDiv.classList.add("detail");
+    tdDetail.textContent = "Détail";
+    tdDetailDiv.appendChild(tdDetail);
+
+    // Add cells in row
+    tr.append(tdNom, tdPrenom, tdVille, tdDetailDiv);
+    return tr;
 }
